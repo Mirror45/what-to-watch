94ee1beba822114edc65ebf1b2a5314ace3e923c
Mirror <yllikano@gmail.com>
2025-08-27 09:23:58 +0300

    refactor(app): update player/[id]/page.tsx

--- src/app/player/[id]/page.tsx
+++ src/app/player/[id]/page.tsx
@@ -1,9 +1,35 @@
-import { JSX } from 'react';
+'use client';
 
-interface PlayerPageProps {
-  params: { id: string };
-}
+import { useParams } from 'next/navigation';
+import { JSX, useEffect } from 'react';
 
-export default function PlayerPage({ params }: PlayerPageProps): JSX.Element {
-  return <h1>Movie Player ID: {params.id}</h1>;
+import { Loading } from '@/components/Loading';
+import { VideoPlayer } from '@/components/VideoPlayer'; // Мы создадим этот компонент
+import { useAppDispatch, useAppSelector } from '@/store/hooks';
+import { selectCurrentFilm } from '@/store/selectors/films/films';
+import { clearCurrentFilm } from '@/store/slices/films/filmSlice';
+import { fetchFilmById } from '@/store/slices/films/filmThunks';
+
+export default function PlayerPage(): JSX.Element {
+  const params = useParams();
+  const filmId = params.id as string;
+  const dispatch = useAppDispatch();
+
+  const film = useAppSelector(selectCurrentFilm);
+  const isLoading = useAppSelector((state) => state.films.isCurrentFilmLoading);
+
+  useEffect(() => {
+    if (filmId) {
+      dispatch(fetchFilmById({ id: filmId }));
+    }
+    return () => {
+      dispatch(clearCurrentFilm());
+    };
+  }, [filmId, dispatch]);
+
+  if (isLoading || !film) {
+    return <Loading />;
+  }
+
+  return <VideoPlayer film={film} />;
 }
