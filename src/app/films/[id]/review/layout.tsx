1e9c8e6eb6fe131cd0f065cc5ece88e0d0dfd9a5
Mirror <yllikano@gmail.com>
2025-08-27 08:47:06 +0300

    refactor(app): update films/[id]/review/layout.tsx

--- src/app/films/[id]/review/layout.tsx
+++ src/app/films/[id]/review/layout.tsx
@@ -1,25 +1,14 @@
 'use client';
 
-import { useParams } from 'next/navigation';
 import { JSX } from 'react';
-
-import Header from '@/components/Header';
-import { films } from '@/mocks/films';
 
 interface AddReviewLayoutProps {
   children: React.ReactNode;
 }
 
 export default function AddReviewLayout({ children }: AddReviewLayoutProps): JSX.Element {
-  const params = useParams();
-  const filmId = params.id as string;
-
-  const film = films.find((film) => film.id === filmId);
-  const filmTitle = film?.title ?? 'Unknown Film';
-
   return (
     <div>
-      <Header pageTitle="Add review" showBreadcrumbs={true} filmTitle={filmTitle} filmId={filmId} />
       <main>{children}</main>
     </div>
   );
